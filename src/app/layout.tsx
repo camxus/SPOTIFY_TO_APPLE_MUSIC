9e02172752083d221575db554bc958ad35b8f1e2
STcamillus <camillus.konkwo@sigtech.com>
2023-07-11 02:11:37 +0100

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 "use client"
 
 import "./globals.css"
-import type { GetStaticProps } from "next"
 import { Inter } from "next/font/google"
-import { useEffect } from "react"
-import { useCookies } from "react-cookie"
-import { getAuthToken } from "./utils/auth"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,26 +12,12 @@ const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
-  token,
 }: {
   children: React.ReactNode
-  token: string
 }) {
-  const [cookies, setCookie, removeCookie] = useCookies(["bearer"])
-
-  useEffect(() => {
-    setCookie("bearer", token)
-  }, [setCookie, token])
-
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
-
-export const getInitialProps = async () => {
-  const token = await getAuthToken()
-  return { props: { token } }
-}
- 
