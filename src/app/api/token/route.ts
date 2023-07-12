d826c4faa879bf7dac60b689927d459d42448c81
Camillus Konkwo <camillus.konkwo@gmail.com>
2023-07-12 11:14:54 +0100

    Update route.ts

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -4,21 +4,25 @@ import { NextResponse } from "next/server"
 import qs from "qs"
 
 export async function GET() {
-  const {
-    data: { access_token },
-  } = await axios.post<{ access_token: string }>(
-    "https://accounts.spotify.com/api/token",
-    qs.stringify({ grant_type: "client_credentials" }),
-    {
-      headers: {
-        Authorization:
-          `Basic ${encode(
-            `${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`
-          )}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  )
-
-  return NextResponse.json({ access_token })
+  try {
+    const {
+      data: { access_token },
+    } = await axios.post<{ access_token: string }>(
+      "https://accounts.spotify.com/api/token",
+      qs.stringify({ grant_type: "client_credentials" }),
+      {
+        headers: {
+          Authorization:
+            `Basic ${encode(
+              `${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`
+            )}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    )
+    if (!process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID || !process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET) throw new Error("env undefined"))
+    return NextResponse.json({ access_token }, { status: 200 })
+  } catch (e: any) {
+    return NextResponse.json(e.message, { status: 500 })
+  }
 }
